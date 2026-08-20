161851b50fc38606630dfc07811262655399e459
urlun0404 <urlun0404@gmail.com>
2026-08-21 00:08:09 +0800

    feat: 新增 /api router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const healthCheckRouter = require("./health-check");
+const apiRouter = require("./api");
 
 router.use("/healthcheck", healthCheckRouter);
+router.use("/api", apiRouter);
 
 module.exports = router;
