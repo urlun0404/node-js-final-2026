4d4860061b183bc128068615655764a50fedbc39
urlun0404 <urlun0404@gmail.com>
2026-08-20 23:50:16 +0800

    feat: 新增 express 基本環境建置

---
+++ backend/bin/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.use(function (req, res, next) {
+  res.status(404).json({ status: "failed", message: "找不到該路由" });
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res
+    .status(500)
+    .json({ status: "failed", message: "伺服器發生錯誤，請稍候再試。" });
+});
+
+module.exports = app;
